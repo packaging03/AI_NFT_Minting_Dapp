3ae05b6e94ff1057960520d27ced84fcae68b368
Adeyemi Olaoye <yhemi06@gmail.com>
2024-10-01 23:03:31 +0100

    updates on the AI nft

--- src/App.js
+++ src/App.js
@@ -1,37 +1,98 @@
-import { useState, useEffect } from 'react';
-import { NFTStorage, File } from 'nft.storage'
-import { Buffer } from 'buffer';
-import { ethers } from 'ethers';
-import axios from 'axios';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState, useEffect } from "react";
+import { NFTStorage, File } from "nft.storage";
+import { Buffer } from "buffer";
+import { ethers } from "ethers";
+import axios from "axios";
 
 // Components
-import Spinner from 'react-bootstrap/Spinner';
-import Navigation from './components/Navigation';
+import Spinner from "react-bootstrap/Spinner";
+import Navigation from "./components/Navigation";
 
 // ABIs
-import NFT from './abis/NFT.json'
+import NFT from "./abis/NFT.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 function App() {
-  const [provider, setProvider] = useState(null)
-  const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState(null);
 
   const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
-  }
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const imageData = createImage();
+  };
+
+  const createImage = async () => {
+    const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2`;
+
+    const response = await axios({
+      url: URL,
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_HUGGING_FACE_API_KEY}`,
+        Accept: "application/json",
+        "Context-Type": "application/json",
+      },
+      data: JSON.stringify({
+        inputs: description,
+        options: { wait_for_model: true },
+      }),
+      responseType: "arraybuffer",
+    });
+    const type = response.headers["content-type"];
+    const data = response.data;
+    const base64Data = Buffer.from(data).toString("base64");
+    const img = `data:${type};base64,` + base64Data;
+    setImage(img);
+    return data;
+  };
 
   useEffect(() => {
-    loadBlockchainData()
-  }, [])
+    loadBlockchainData();
+  }, []);
 
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
-      <p>Edit App.js to get started.</p>
+      <div className="form">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            placeholder="Add a name..."
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Add a description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input type="submit" value="Create & Mint" />
+        </form>
+        <div className="image">
+          <img src={image} alt="AI generated pictures" />
+        </div>
+      </div>
 
+      <p>
+        View&nbsp;
+        <a href="" target="_blank" rel="noreferrer">
+          Metadata
+        </a>
+      </p>
     </div>
   );
 }
