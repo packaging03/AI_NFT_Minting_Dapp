14f1a02bd9213a6e6c17b0bd3ff32c9b302ffba7
Adeyemi Olaoye <yhemi06@gmail.com>
2024-10-05 20:50:12 +0100

    restore code

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ import NFT from "./abis/NFT.json";
 
 // Config
 import config from "./config.json";
+// import { wait } from "@testing-library/user-event/dist/utils";
+// import { type } from "os";
+// import { network } from "hardhat";
 
 function App() {
   const [provider, setProvider] = useState(null);
